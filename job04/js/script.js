48cb38d1f3d89e782d7999443a9dc1b7598088d3
Ryan Hafid Alvie Hamdi <65152655+ryanhafid@users.noreply.github.com>
2022-04-05 21:32:48 +0700

    Ditambah jQuery
    Jobsheet 4 Memodifikasi web yang sudah dibuat dengan ditambahi jQuery

---
+++ job04/js/script.js
@@ -0,0 +1,107 @@
+// memastikan file sudah dilolad dgn sempurna
+$(document).ready(function () {
+
+    // navbar
+    $('#navbar1').click(function () {
+        $('.menu').slideToggle();
+    });
+
+
+    $('.menu2').hide();
+
+    $('#ryan').click(function () {
+        $('.menu2').slideToggle();
+    });
+
+
+    // akhir navbar============
+
+
+
+    $('#kirimPesan').css({
+        'color': '#12806a',
+        'fontFamily': 'BebasNeue',
+        'fontFamily': 'sans-serif',
+        'text-align': 'center'
+    });
+
+    $('#kontak').mouseenter(function () {
+        $('#kontak').css('color', 'yellow');
+    });
+
+    $('#kontak').mouseleave(function () {
+        $('#kontak').css('color', 'white');
+    });
+
+    $('.btn-primary').click(function () {
+        var nama = $('#nama').val();
+        alert("Terima kasih telah mengirim pesan ini  " + nama) //aya notif diluhurna//
+
+    });
+
+    $('.content').mouseenter(function () {
+        $('.content').css({
+            'border-bottom': '5px solid #3498db'
+        });
+    });
+
+    $('.content').mouseleave(function () {
+        $('.content').css({
+            'border-bottom': '0 solid #3498db'
+        });
+    });
+
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            // Store hash
+            var hash = this.hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+    // ========== marquee ==========
+    $('marquee').mouseenter(function () {
+        $('marquee').css('color', 'blue');
+        $('marquee').stop();
+    });
+
+    $('marquee').mouseleave(function () {
+        $('marquee').css('color', 'inherit');
+    });
+
+
+    var kelas1 = $('.menu').offset().top;
+
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() >= kelas1) {
+            $('.menu').addClass('kelas1');
+            $('.wrap').addClass('kelas2');
+
+        } else {
+            $('.menu').removeClass('kelas1');
+            $('.wrap').removeClass('kelas2');
+        }
+    })
+
+
+
+
+
+
+
+
+});
